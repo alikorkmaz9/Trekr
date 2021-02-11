390c17018fb89f63b433deb6cf01cf228c29e373
alikorkmaz9 <74213404+alikorkmaz9@users.noreply.github.com>
2021-02-11 11:58:06 +0300

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Ali Korkmaz on 11.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
